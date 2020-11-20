45156948b7efc9e5bfe6e47d6e089bf0dcf18277
amaechiw8 <62082093+amaechiw8@users.noreply.github.com>
2020-11-20 13:45:39 -0500

    Add files via upload

---
+++ TestSeq.java
@@ -0,0 +1,34 @@
+package com.Tester;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "TestSequence")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TestSeq{
+    private List<? extends TestStep> testSeq;
+
+    public TestSeq(){}
+    public TestSeq(List<? extends TestStep> testSeq){
+        super();
+        this.testSeq = testSeq;
+    }
+
+    public List<? extends TestStep> getTestSeq(){
+        return testSeq;
+    }
+
+    public void setTestSeq(List<? extends TestStep> testSeq){
+        this.testSeq = testSeq;
+    }
+
+    public void printSeq() {
+        for (TestStep testStep : testSeq)
+        {
+            System.out.println(testStep.toString());
+        }
+    }
+}
---
+++ TestStep.java
@@ -0,0 +1,26 @@
+package com.Tester;
+
+public abstract class TestStep {
+    private String testStep;
+
+    public TestStep() {}
+    public TestStep(String testStep){
+        super();
+        this.testStep = testStep;
+    }
+
+    public String getTestStep(){
+           return testStep;
+    }
+
+    public void setTestStep(String testStep) {
+        this.testStep = testStep;
+    }
+
+    @Override
+    public String toString() {
+        return "TestStep{" +
+                "testStep='" + testStep + '\'' +
+                '}';
+    }
+}
